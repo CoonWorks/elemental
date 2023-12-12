bbdab30e7812b1ab91e6126fe6de8c75425061a4
José Ignacio Bravo <josebravo_2009@hotmail.com>
2023-12-12 20:33:03 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,6 @@
-
+function TipoSelector(n){
+  if(document.getElementsByClassName(n).class != "toggle"){
+    document.getElementsByClassName(n).classList.add('toggle');
+  } else{
+    document.getElementsByClassName(n).classList.remove('toggle');
+}
