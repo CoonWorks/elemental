50b4f5a9361a47103386039614f10bce7c2c2008
José Ignacio Bravo <josebravo_2009@hotmail.com>
2023-12-15 10:53:28 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,3 +1,13 @@
+function toggler(n){
+  if(j = 0){
+    document.getElementById('tabla'+n).style.transform="scale(1,1)";
+    j = j + 1;
+  } else{
+    document.getElementById('tabla'+n).style.transform="scale(0,1)";
+    j = j - 1;
+  }
+}
+
 function TipoSelector(n){
   document.getElementsByClassName(n).classList.add('toggle');
 }
