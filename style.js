4a6bfee6d7e7820ece215a6dd19e3f9d0a011887
José Ignacio Bravo <josebravo_2009@hotmail.com>
2023-12-15 18:35:38 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,3 +1,7 @@
+var jQueryScript = document.createElement('script');
+jQueryScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+
 function TypeSelector(n){
   if(document.getElementsByClassName(n).classList.contains('toggle')){
     document.getElementsByClassName(n).classList.remove('toggle')
