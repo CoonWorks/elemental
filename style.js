36431f313bdd67bec25222093b87c560ac614d20
José Ignacio Bravo <josebravo_2009@hotmail.com>
2023-12-15 18:12:59 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,23 +1,17 @@
-var j = 0;
-
-function toggler(){
-  if(j = 0){
-    document.getElementById('tablaLan').style.transform="scale(1,1)";
-    j = j + 1;
+function TypeSelector(n){
+  if(document.getElementsByClassName(n).classList.contains('toggle')){
+    document.getElementsByClassName(n).classList.remove('toggle')
   } else{
-    document.getElementById('tablaLan').style.transform="scale(0,1)";
-    j = j - 1;
+    document.getElementsByClassName(n).classList.add('toggle');
   }
 }
 
-function TipoSelector(n){
-  document.getElementsByClassName(n).classList.add('toggle');
+function toggler(n){
+  if(document.getElementById('tabla'+n).classList.contains('extender')){
+    document.getElementById('tabla'+n).classList.remove('extender')
+  } else{
+    document.getElementById('tabla'+n).classList.add('extender');
+  }
 }
 
-function RemoveSelector(n){
-  document.getElementsByClassName(n).classList.remove('toggle');
-}
-
-document.getElementById('togglerLan').addEventListener('click',toggler());
-document.getElementById('selector-'+t).addEventListener('mouseover',TipoSelector(t));
-document.getElementById('selector-'+t).addEventListener('mouseout',RemoveSelector(t));
+document.getElementById('toggler'+n).addEventListener('click',toggler(n));
