14169f33d37a526b5b36a6de9a8c42264d3f8dcd
José Ignacio Bravo <josebravo_2009@hotmail.com>
2023-12-15 19:07:38 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,3 +1,5 @@
+var contando = 0;
+
 function TypeSelector(n){
   if(document.getElementsByClassName(n).classList.contains('toggle')){
     document.getElementsByClassName(n).classList.remove('toggle')
@@ -7,12 +9,12 @@ function TypeSelector(n){
 }
 
 function toggler(){
-  if(var contando = 0){
+  if(contando = 0){
     document.getElementById('tablaLan').style.transform="scaleX(1)";
-    var contando = 1;
+    contando = 1;
   } else{
     document.getElementById('tablaLan').style.transform="scaleX(0)";
-    var contando = 0;
+    contando = 0;
   }
 }
 
